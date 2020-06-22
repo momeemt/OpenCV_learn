cbd2eebaf860468220935ea10e16574ecd51741e
Momeemt <momiimt@gmail.com>
2020-06-22 12:17:25 +0900

    ✨ ファイルから動画を読み込む

--- tottori-u-opencv/src/module/opencv_mvshow.py
+++ tottori-u-opencv/src/module/opencv_mvshow.py
@@ -2,8 +2,24 @@ import cv2
 
 
 def main():
+    read_video()
+
+
+def video_caps():
     cap = cv2.VideoCapture(0)
     while True:
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('frame', gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def read_video():
+    cap = cv2.VideoCapture('../assets/mov/view.mp4')
+    while cap.isOpened():
         ret, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         cv2.imshow('frame', gray)

d2e69d8fc97ccdc67e2a8ccf05768a9ca210bbce
Momeemt <momiimt@gmail.com>
2020-06-22 10:58:53 +0900

    ✨ Matplotlibを使った画像の表示
    グレースケールで表示

--- tottori-u-opencv/src/main.py
+++ tottori-u-opencv/src/main.py
@@ -1,11 +1,8 @@
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
 img = cv2.imread('../assets/img/momeemt.jpg')
-cv2.imshow('image', img)
-k = cv2.waitKey(0)
-if k == 27:
-    cv2.destroyAllWindows()
-elif k == ord('s'):
-    cv2.imwrite('momee_icon.jpg', img)
-    cv2.destroyAllWindows()
+plt.imshow(img, cmap='gray', interpolation='bicubic')
+plt.xticks([]), plt.yticks([])
+plt.show()
